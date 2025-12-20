b1097334228a9a9b0fd728726e4741edf9d90dc4
jasgrg <your-email@example.com>
2025-12-20 07:49:30 -0500

    Fixed sound path

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -27,7 +27,7 @@ const Home: React.FC = () => {
                 key={track.id}
                 title={track.title}
                 artist={track.artist}
-                filePath={`/music/${track.filename}`}
+                filePath={`soundstream/public/music/${track.filename}`}
               />
             ))}
           </div>
